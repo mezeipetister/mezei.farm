d2e6f06df96e9f91234375de6f69b90d84634498
mezeipetister2 <mezeipetister@gmail.com>
2017-05-08 13:55:10 +0200

    multi language support added

--- routes.js
+++ routes.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-// Test for language detection.
-// router.get('*', function(req, res, next) {
-//   var isItHu = req.acceptsLanguages('hu');
-//   if(isItHu) {
-//     console.log('Hungarian');
-//   } else {
-//     console.log('Not Hungarian');
-//   }
-// });
+// Managing multiple languages detecting the main browser language
+router.use(function(req, res, next) {
+  var isItHu = req.acceptsLanguages('hu');
+  if(isItHu) {
+    var lang = 'default';
+  } else {
+    var lang = 'english';
+  }
+  var fs = require('fs');
+  req.languageFile = JSON.parse(fs.readFileSync('lang/' + lang + '.json', 'utf8'));
+  next();
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Üdvözöljük', path: req.url });
+  res.render('index', { title: req.languageFile["title-index"], path: req.url, text: req.languageFile });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Rólunk', path: req.url });
+  res.render('about', { title: req.languageFile["title-about"], path: req.url });
 });
 
 router.get('/products', function(req, res, next) {
-  res.render('products', { title: 'Termékeink', path: req.url });
+  res.render('products', { title: req.languageFile["products"], path: req.url });
 });
 
 module.exports = router;
