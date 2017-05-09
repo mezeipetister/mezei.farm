96bcb59afdf56a8e1387a0ca815dce7257860316
mezeipetister2 <mezeipetister@gmail.com>
2017-05-09 16:57:14 +0200

    test added, not stable

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes');
-var sitemap = require('express-sitemap')();
+var sitemap = require('express-sitemap')({
+  url: require('os').hostname()
+});
 
 var app = express();
 
